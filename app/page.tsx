87e52f519c78e57b8835ac6a3c94fd1df1b84dc0
ohnowho <ohnowho@outlook.com>
2023-06-19 15:27:42 +0900

    dev: parallax

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import "/styles/global.css";
+import Zswiper from "@/components/zswiper";
 export default function Home() {
   return (
     <main>
-      <div><iframe src="https://gifer.com/embed/wau" width="100%" height="100%" frameBorder="0" allowFullScreen></iframe></div>
+      {/* <div><iframe src="https://gifer.com/embed/wau" width="100%" height="100%" frameBorder="0" allowFullScreen></iframe></div> */}
+      <Zswiper></Zswiper>
     </main>
   )
 }
--- components/zswiper/index.tsx
+++ components/zswiper/index.tsx
@@ -1,14 +1,70 @@
 "use client";
 import { useState, useEffect } from "react";
 import styles from "./index.module.scss";
+import { animated, useSpring } from '@react-spring/web'
+import  { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
+function MyComponent() {
+  return (
+    <Parallax pages={1} style={{ top: '0', left: '0' }}>
+      <ParallaxLayer offset={0} speed={2.5}>
+        <p>Parallax</p>
+      </ParallaxLayer>
+    </Parallax>
+  )
+}
 const cdnHost = `/images`;
+const arr = Array.from({ length: 12 }, (v, k) => {
+  return `https://img.ginzaxiaoma.com/images/tenthAnniv/fresh0609/freshnew/sharestory-${k}.webp`;
+});
+function spArr(arr, num) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; ) {
+    newArr.push(arr.slice(i, (i += num)));
+  }
+  return newArr;
+}
+let arrs = spArr(arr, 4);
+const uls = [
+  {
+    imgs: arrs[0]
+    
+  }
+]
 
 const Zswiper = () => {
+  const [offset, setOffset] = useState(0);
+  const sty = useSpring({
+    y:  24,
+  })
+  function handleScroll(e) {
+    console.log(e);
+    e.preventDefault();
+  }
+  function handleWheel(e) {
+    console.log(e);
+    e.preventDefault();
+  }
   useEffect(() => {
+    // window.addEventListener("wheel", handleScroll,{passive:false});
   }, []);
   return (
     <div className={styles.zswiper}>
+      <div className={styles.title} id="h1">
+        <h1>GZXM</h1>
+        <h2>Hermès Boutique Tokyo</h2>
+      </div>
+      <div className={styles.wrap}>
+        {arrs.map((row, rowi) => (
+            <ul key={rowi}>
+              {row.map((item,itemi) => (
+                <li key={itemi}>
+                  <img src={item}></img>
+                </li>
+              ))}
+            </ul>
+        ))}
+      </div>
     </div>
   );
 };
