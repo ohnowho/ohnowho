36437bfa343ec43a1aba8535021210868e2cff07
ohnowho <ohnowho@outlook.com>
2023-06-19 17:08:21 +0900

    dev: fixed

--- components/zswiper/index.tsx
+++ components/zswiper/index.tsx
@@ -1,18 +1,9 @@
 "use client";
 import { useState, useEffect } from "react";
 import styles from "./index.module.scss";
-import { animated, useSpring } from '@react-spring/web'
-import  { Parallax, ParallaxLayer } from '@react-spring/parallax';
+import { animated, useSpring, useScroll } from "@react-spring/web";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
-function MyComponent() {
-  return (
-    <Parallax pages={1} style={{ top: '0', left: '0' }}>
-      <ParallaxLayer offset={0} speed={2.5}>
-        <p>Parallax</p>
-      </ParallaxLayer>
-    </Parallax>
-  )
-}
 const cdnHost = `/images`;
 const arr = Array.from({ length: 12 }, (v, k) => {
   return `https://img.ginzaxiaoma.com/images/tenthAnniv/fresh0609/freshnew/sharestory-${k}.webp`;
@@ -27,16 +18,16 @@ function spArr(arr, num) {
 let arrs = spArr(arr, 4);
 const uls = [
   {
-    imgs: arrs[0]
-    
-  }
-]
+    imgs: arrs[0],
+  },
+];
 
 const Zswiper = () => {
   const [offset, setOffset] = useState(0);
+  const { scrollYProgress } = useScroll();
   const sty = useSpring({
-    y:  24,
-  })
+    y: 24,
+  });
   function handleScroll(e) {
     console.log(e);
     e.preventDefault();
@@ -49,21 +40,26 @@ const Zswiper = () => {
     // window.addEventListener("wheel", handleScroll,{passive:false});
   }, []);
   return (
-    <div className={styles.zswiper}>
-      <div className={styles.title} id="h1">
-        <h1>GZXM</h1>
-        <h2>Hermès Boutique Tokyo</h2>
-      </div>
-      <div className={styles.wrap}>
-        {arrs.map((row, rowi) => (
-            <ul key={rowi}>
-              {row.map((item,itemi) => (
+    <div className={styles.countainer}>
+      <div className={styles.zswiper}>
+        <div className={styles.title} id="h1">
+          <h1>GZXM</h1>
+          <h2>Hermès Boutique Tokyo</h2>
+        </div>
+        <div className={styles.wrap}>
+          {arrs.map((row, rowi) => (
+            <ul id={`row-${rowi}`} key={rowi}>
+              {row.map((item, itemi) => (
                 <li key={itemi}>
                   <img src={item}></img>
                 </li>
               ))}
             </ul>
-        ))}
+          ))}
+        </div>
+      </div>
+      <div id="zswiper-scroll" className={styles.scroller}>
+        <span></span>
       </div>
     </div>
   );
