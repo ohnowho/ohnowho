7232a415485b9e998c8cd5235cd13c86b86c22d5
ohnowho <ohnowho@outlook.com>
2023-08-02 18:36:06 +0900

    feat: remote config

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import "/styles/global.css";
 import Zswiper from "@/components/zswiper";
-export default function Home() {
+import axios from 'axios';
+export default async function Home() {
+  let url =  "https://api.vika.cn/fusion/v1/datasheets/dst3H956L7YLEjNTWH/records?viewId=viws5XCBnahjy&fieldKey=name"
+  const res = await axios.get(url,{
+    headers: {
+      Authorization: `Bearer uskKG4iSt25VxKVCqFsUTfq`
+    }
+  });
   return (
     <main id="main">
       {/* <div><iframe src="https://gifer.com/embed/wau" width="100%" height="100%" frameBorder="0" allowFullScreen></iframe></div> */}
-      <Zswiper></Zswiper>
+      <Zswiper res={res.data}></Zswiper>
     </main>
   )
 }
